7b4097beb815dbe3eac75d64c107c7ce2f1cdacc
Tarun <trnadtya@gmail.com>
2024-07-31 22:06:34 +0530

    Allows user to either take a photo or upload a photo.Once done, the image will be converted to a grayscale image. This webapp can be further developed to be used in ML or image processing applications.

--- web.py
+++ web.py
@@ -1,15 +1,25 @@
 import streamlit as st
 from PIL import Image
 
-with st.expander("Start Camera"):
-    #Start the camera
+st.subheader("Color to Grayscale Converter")
+
+# Create a file uploader component allowing the user to upload a file
+uploaded_image = st.file_uploader("Upload Image")
+
+with st.expander("Start camera"):
     camera_image = st.camera_input("Camera")
+
 if camera_image:
-    #Create a pillow image instance
+    # Supply camera_image to convert_image to get the grayscale version
     img = Image.open(camera_image)
+    gray_camera_img = img.convert('L')
+    st.image(gray_camera_img)
 
-    #Convert the pillow image to grayscale
-    gray_img = img.convert("L")
-
-    #Render the image on the webpage
-    st.image(gray_img)
+# Check if the image exists meaning the user has uploaded a file
+if uploaded_image:
+    # Open the user uploaded image with PIL
+    img = Image.open(uploaded_image)
+    # Convert the image to grayscale
+    gray_uploaded_img = img.convert('L')
+    # Display the grayscale image on the webpage
+    st.image(gray_uploaded_img)
